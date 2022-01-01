92d1b6c47d1bfbd47afcae45965bc0a687da60bf
DavidNaizheZhou <david.zhou@tuwien.ac.at>
2022-01-01 15:04:17 +0100

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,8 @@ html_theme_path = ["_themes", ]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "_static/img/imws-logo_tu.png"
+# html_logo = "_static/img/imws-logo_tu.png"
+html_logo = "_static/img/stanpy_logo.png"
 
 # -- Options for HTMLHelp output ---------------------------------------
 
